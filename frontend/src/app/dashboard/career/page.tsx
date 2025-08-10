e305e629b723ce263f25934348a7229bf56deb9a
Moeein Aali <moeeeinaali@gmail.com>
2025-08-10 15:24:07 +0330

    add api origin to client

--- frontend/src/app/dashboard/career/page.tsx
+++ frontend/src/app/dashboard/career/page.tsx
@@ -1,35 +1,36 @@
 'use server';
 
-import { fetchEmailAndProfile } from '../../../lib/api/dashboard/profile';
+import {fetchEmailAndProfile} from '../../../lib/api/dashboard/profile';
 import ProfileCompletionWarning from '../register/ProfileCompletionWarning';
 import React from 'react';
 import CareerForm from './CareerForm';
 import {
-  getMyApplications,
-  getPositions,
+    getMyApplications,
+    getPositions,
 } from '../../../lib/api/dashboard/career';
 
 export default async function CareerPanel() {
-  const [
-    {
-      profile: { firstName, lastName, phoneNumber },
-    },
-    positions,
-    applications,
-  ] = await Promise.all([
-    fetchEmailAndProfile(),
-    getPositions(),
-    getMyApplications(),
-  ]);
-  const isProfileComplete = Boolean(firstName && lastName && phoneNumber);
-  return (
-    <div className="space-y-7 p-6">
-      {!isProfileComplete && <ProfileCompletionWarning />}
-      <CareerForm
-        isAuthenticated={true}
-        brands={positions}
-        applications={applications}
-      />
-    </div>
-  );
+    const [
+        {
+            profile: {firstName, lastName, phoneNumber},
+        },
+        positions,
+        applications,
+    ] = await Promise.all([
+        fetchEmailAndProfile(),
+        getPositions(),
+        getMyApplications(),
+    ]);
+    const isProfileComplete = Boolean(firstName && lastName && phoneNumber);
+    return (
+        <div className="space-y-7 p-6">
+            {!isProfileComplete && <ProfileCompletionWarning/>}
+            <CareerForm
+                API_ORIGIN={process.env.API_ORIGIN}
+                isAuthenticated={true}
+                brands={positions}
+                applications={applications}
+            />
+        </div>
+    );
 }
